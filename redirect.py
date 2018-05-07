08743cebdab3f76a8b497735276b0ec80cc37941
daisie_local <daisieh@gmail.com>
2018-05-06 22:39:02 -0700

    test

---
+++ redirect.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return 'To validate a file, send a POST request with the file as "multipart/form-data" to https://validatemyfile.herokuapp.com/validate. You can do this with curl from the command line: curl https://validatemyfile.herokuapp.com/validate  -F "file=@test.fasta"'
+
+@app.route('/pyMesquiteFeedback', methods=['GET'])
+def feedback():
