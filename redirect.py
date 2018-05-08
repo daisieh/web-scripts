75e53f7a6349432257b091742735794b38fc8bcb
daisie_local <daisieh@gmail.com>
2018-05-08 11:00:30 -0700

    test

--- redirect.py
+++ redirect.py
@@ -1,5 +1,9 @@
 import flask
 from werkzeug.utils import secure_filename
+import sys
+import os
+import urllib 
+from time import strftime, time
 
 app = flask.Flask(__name__)
 
@@ -16,9 +20,37 @@ def feedback():
 @app.route('/pyMesquiteStartup', methods=['POST', 'GET'])
 def startup():
 #<Request 'https://mesquiteproject.herokuapp.com/pyMesquiteStartup?build=	PreRelease-875	OS+%3D	Mac+OS+X	10.11.6	java+%3D	1.8.0_111	Oracle+Corporation' [GET]>
-#     if flask.request.method == 'POST':
-    app.logger.error('%s', flask.request)
-    return flask.render_template('error.html', error=flask.request)
+    
+    ip = flask.request.remote_addr
+    raw = flask.request.query_string
+    app.logger.error('%s', raw)
+
+    # the file we'll be appending version information to 
+    feedbackfile = "version_feedback.log"
+
+#     open the file w/ appending mode 
+#     FILE = open(feedbackfile, "a")
+# 
+#     split the query string by the value delimiter, &. 
+#     vallist = raw.split('&')
+# 
+#     print the start of the line with current time
+#     FILE.write('[%s] - ' % strftime("%Y-%m-%d %H:%M:%S"))
+#     FILE.write('ip = \t%s' % (ip))
+#     FILE.write('\t')
+#            
+#     for each value in the query string, print it after the line start           
+#     for k in vallist:
+#         tuple = k.split('=')
+#         FILE.write('%s = %s, ' % (tuple[0], urllib.unquote_plus(tuple[1])))
+#     
+#     terminate the line     
+#     FILE.write('\n')
+# 
+#     flush and close 
+#     FILE.flush()
+#     FILE.close()
+    return flask.render_template('startup.html')
 
 @app.route('/pyMesquiteBeans', methods=['POST', 'GET'])
 def beans():
