69f36ea851f7aa6ba3734f359d8ef73641583d4b
daisie_local <daisieh@gmail.com>
2018-05-09 15:49:27 -0700

    test

--- redirect.py
+++ redirect.py
@@ -20,7 +20,7 @@ def catch_all(path):
     elif path == 'pyMesquiteBeans':
         beans()
     else:
-        return flask.redirect('mesquiteproject.github.io')
+        return flask.redirect('https://mesquiteproject.github.io')
 
 def feedback(prerelease=False):
     if prerelease:
